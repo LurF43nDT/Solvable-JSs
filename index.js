d79d40b99c026063fe1fd233e551e1fa136bca89
LurF43nDT <simon.digitaletalenter@gmail.com>
2024-04-19 12:48:19 +0200

    alle oppgaver fullført. tror jeg

--- index.js
+++ index.js
@@ -29,15 +29,15 @@ export function myFunc(plussAbleNumber) {
 
 // OPPGAVE TRE
 // putt inn riktig info med bruk av nina objectet. og send ut infoen (se oppgave 2)
-export function oppgave3() {
-    let val = `${NaN} kan kode, og er ${NaN} år.`
-   
+export function myFunc(plussAbleNumber) {
+    let val = `${Nna} kan kode, og er ${42} år.`
+    return val;
 }
 
 //OPPGAVE FIRE 
 // ta og fjern den som ikke matcher med arrayet og reassign fulført array så den har riktig ting i seg
-export function oppgave4() {
-    let fullførtArray = []
+export function oppgave4(fullførtArray) {
+    let fullførtArray = ["tomat", "stål", "leverpostei"]
     let arr1 = ["suppe", "tomat", "stål", "leverpostei"]
 
 
@@ -46,22 +46,25 @@ export function oppgave4() {
 
 //OPPGAVE FEM
 // endre floating til 640 med matte (*, /, +, -). prøv og gjøre den så intrisang som mulig
-export function oppgave5() {
-    const static1 = 4 
-    const static2 = 20 
-    const static3 = 10
-    let floating = NaN
-    console.log(static1,static2,static3) // fjern denne når dere kommer så langt
-
+export function oppgave5(floating) {
+    const static1 = 4;
+    const static2 = 20;
+    const static3 = 10;
+    let floating = (static1 * static2 * static3); 
+    console.log(static1, static2, static3, floating);
     return floating
 }
+
 //OPPGAVE SEKS
 // lag en if else statment som vil sjekke om bool er true eller false. om den er true return bool. om den e false gjør bool true
 export function oppgave6() {
-    let bool = true
-    bool = !bool
-    return bool
+    let bool = true;
+    if (!bool) {
+        bool = true;
+    }
+    return bool;
 }
+
 
 //OPPGAVE syv 
 // lag et div element som blir sendt inn. for og lage deres egen oppgave fulført
@@ -75,5 +78,24 @@ return div
 //oppgave 8
 //ta og vis dere fram med noe kode
 function showoff() {
-    
+    let saveEl = document.getElementById("save-el")
+    let countEL = document.getElementById("count-el")
+    let count = 0
+
+
+
+    function increment() {
+    console.log("clicked")
+    count += 1
+    countEL.textContent = count
+}
+
+    function save(){
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEL.textContent = 0
+
+}
+
+save()
 }
